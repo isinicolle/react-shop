6aab48db27e4372214cd9c79f5730734f8fbbce2
Isis Zapata <isis.zapata.hn@gmail.com>
2022-09-06 21:27:25 -0600

    Update login.jsx

--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 
 const login = () => {
   return (
-    <View>
-      <Text>login</Text>
-    </View>
-  )
+
+            <div classNameName="login">
+                <div classNameName="form-container">
+                <img src="./logos/logo_yard_sale.svg" alt="logo" classNameName="logo"/>
+
+                <h1 classNameName="title">Create a new password</h1>
+                <p classNameName="subtitle">Enter a new passwrd for yue account</p>
+
+                <form action="/" className="form">
+                    <label for="password" className="label">Password</label>
+                    <input type="password" id="password" placeholder="*********" className="input input-password"/>
+
+                    <label for="new-password" classNameName="label">Password</label>
+                    <input type="password" id="new-password" placeholder="*********" classNameName="input input-password"/>
+
+                    <input type="submit" value="Confirm" className="primary-button login-button"/>
+                </form>
+                </div>
+            </div>
+            
+            
+        )
 }
 
-export default login
+export default login;
