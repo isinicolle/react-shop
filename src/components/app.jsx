3c66430d6de284a65efa94d01795b910f374c81a
Isis Zapata <isis.zapata.hn@gmail.com>
2022-09-06 20:18:39 -0600

    .
    .

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/global.scss';
 
-return App = () => {
-  return (
-    <h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
-  );
+const App = () => {
+	return (
+		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+	);
 }
 
 export default App;
