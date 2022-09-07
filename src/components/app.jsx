a8d33442ea38141b396e3955e8d88d5ad2a28691
Isis Zapata <isis.zapata.hn@gmail.com>
2022-09-06 22:51:02 -0600

    Update app.jsx
    s eimproto react-router-dom

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
